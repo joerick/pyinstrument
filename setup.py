eb6609591d879135e25516b3eb1d1f71380d1b9d
Joe Rickerby <joerick@mac.com>
2014-03-13 18:26:44 +0000

    Fix distutils distribution

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="pyinstrument",
     version="0.1",
+    description="A Python profiler that records the call stack of the executing code, instead of just the final function in it. Inspired by Apple's Instruments.app",
     long_description=open('README.md').read(),
-    packages=find_packages(),
+    packages=['pyinstrument'],
     include_package_data=True,
     zip_safe=False,
 )
