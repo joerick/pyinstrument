c4a4de7fa531b058987703408a883fc096b57b18
Joe Rickerby <joerick@mac.com>
2014-03-24 23:19:15 +0000

    Remove non-essential django cruft

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -1,10 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
     url(r'^$', 'django_test.views.hello_world'),
-
-    url(r'^admin/', include(admin.site.urls)),
 )
