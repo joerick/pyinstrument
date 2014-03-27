906e97e399f9a9df4e28ec261733b1bf7b731302
Joe Rickerby <joerick@mac.com>
2014-03-27 15:06:16 +0000

    Rearrange source file

--- pyinstrument/profiler.py
+++ pyinstrument/profiler.py
@@ -5,6 +5,101 @@ import inspect
 from operator import attrgetter
 
 
+class Profiler(object):
+    def __init__(self):
+        self.stack_time = {}
+        self.current_call_stack = []
+        self.current_call_stack_start_times = []
+        self.time_spent_in_profiler = 0
+
+    def start(self):
+        for frame_record in inspect.stack()[::-1]:
+            # call the profile function as if we just entered all the frames on the stack
+            self._profile(frame_record[0], 'call', None)
+
+        sys.setprofile(self._profile)
+
+    def stop(self):
+        sys.setprofile(None)
+
+        while len(self.current_call_stack) > 0:
+            # call the profile function as if we just left all the frames on the stack
+            self._profile(None, 'return', None)
+
+    def _profile(self, frame, event, arg):
+        method_time = time.clock()
+
+        if event == 'call':
+            self.current_call_stack.append(self._identifier_for_frame(frame))
+            self.current_call_stack_start_times.append(method_time - self.time_spent_in_profiler)
+        elif event == 'return':
+            stack = tuple(self.current_call_stack)
+            self.current_call_stack.pop()
+
+            frame_start = self.current_call_stack_start_times.pop()
+            frame_duration = method_time - frame_start - self.time_spent_in_profiler
+            
+            self.stack_time[stack] = self.stack_time.get(stack, 0) + frame_duration
+
+        self.time_spent_in_profiler += time.clock() - method_time
+
+    def _identifier_for_frame(self, frame):
+        return '%s\t%s:%i' % (frame.f_code.co_name, frame.f_code.co_filename, frame.f_lineno)
+
+    def root_frame(self):
+        """
+        Returns the parsed results in the form of a tree of Frame objects
+        """
+        if not hasattr(self, '_root_frame'):
+            self._root_frame = Frame()
+
+            # define a recursive function that builds the heirarchy of frames given the
+            # stack of frame identifiers
+            def frame_for_stack(stack):
+                if len(stack) == 0:
+                    return self._root_frame
+
+                parent = frame_for_stack(stack[:-1])
+                frame_name = stack[-1]
+
+                if not frame_name in parent.children:
+                    parent.children[frame_name] = Frame(frame_name, parent)
+
+                return parent.children[frame_name]
+
+            for stack_frame in self.stack_time.iteritems():
+                frame_for_stack(stack_frame[0]).time = stack_frame[1]
+
+            # fix up root_frames's time so Frame's proportion_of_total works correctly
+            for child_frame in self._root_frame.children.values():
+                self._root_frame.time += child_frame.time
+
+        return self._root_frame
+
+    def first_interesting_frame(self):
+        """ 
+        Traverse down the frame heirarchy until a frame is found with more than one child
+        """
+        frame = self.root_frame()
+
+        while len(frame.children.values()) <= 1:
+            frame = frame.children.values()[0]
+
+        return frame
+
+    def starting_frame(self, root=False):
+        if root:
+            return self.root_frame()
+        else:
+            return self.first_interesting_frame()
+
+    def output_text(self, root=False):
+        return self.starting_frame(root).as_text()
+
+    def output_html(self, root=False):
+        return self.starting_frame(root).as_html()
+
+
 class Frame(object):
     """
     Object that represents a frame in the parsed trace
@@ -123,95 +218,3 @@ class Frame(object):
         result += '</div></div>'
 
         return result
-
-
-class Profiler(object):
-    def __init__(self):
-        self.stack_time = {}
-        self.current_call_stack = []
-        self.current_call_stack_start_times = []
-        self.time_spent_in_profiler = 0
-
-    def start(self):
-        for frame_record in inspect.stack()[::-1]:
-            # call the profile function as if we just entered all the frames on the stack
-            self._profile(frame_record[0], 'call', None)
-
-        sys.setprofile(self._profile)
-
-    def stop(self):
-        sys.setprofile(None)
-
-        while len(self.current_call_stack) > 0:
-            # call the profile function as if we just left all the frames on the stack
-            self._profile(None, 'return', None)
-
-    def _profile(self, frame, event, arg):
-        method_time = time.clock()
-
-        if event == 'call':
-            self.current_call_stack.append(self._identifier_for_frame(frame))
-            self.current_call_stack_start_times.append(method_time - self.time_spent_in_profiler)
-        elif event == 'return':
-            stack = tuple(self.current_call_stack)
-            self.current_call_stack.pop()
-
-            frame_start = self.current_call_stack_start_times.pop()
-            frame_duration = method_time - frame_start - self.time_spent_in_profiler
-            
-            self.stack_time[stack] = self.stack_time.get(stack, 0) + frame_duration
-
-        self.time_spent_in_profiler += time.clock() - method_time
-
-    def _identifier_for_frame(self, frame):
-        return '%s\t%s:%i' % (frame.f_code.co_name, frame.f_code.co_filename, frame.f_lineno)
-
-    def root_frame(self):
-        if not hasattr(self, '_root_frame'):
-            self._root_frame = Frame()
-
-            # define a recursive function that builds the heirarchy of frames given the
-            # stack of frame identifiers
-            def frame_for_stack(stack):
-                if len(stack) == 0:
-                    return self._root_frame
-
-                parent = frame_for_stack(stack[:-1])
-                frame_name = stack[-1]
-
-                if not frame_name in parent.children:
-                    parent.children[frame_name] = Frame(frame_name, parent)
-
-                return parent.children[frame_name]
-
-            for stack_frame in self.stack_time.iteritems():
-                frame_for_stack(stack_frame[0]).time = stack_frame[1]
-
-            # fix up root_frames's time so Frame's proportion_of_total works correctly
-            for child_frame in self._root_frame.children.values():
-                self._root_frame.time += child_frame.time
-
-        return self._root_frame
-
-    def first_interesting_frame(self):
-        """ 
-        Traverse down the frame heirarchy until a frame is found with more than one child
-        """
-        frame = self.root_frame()
-
-        while len(frame.children.values()) <= 1:
-            frame = frame.children.values()[0]
-
-        return frame
-
-    def starting_frame(self, root=False):
-        if root:
-            return self.root_frame()
-        else:
-            return self.first_interesting_frame()
-
-    def output_text(self, root=False):
-        return self.starting_frame(root).as_text()
-
-    def output_html(self, root=False):
-        return self.starting_frame(root).as_html()
