cd4b9665d92052bef533c122bc8a22f83878afb6
Joe Rickerby <joerick@mac.com>
2014-06-06 20:12:44 +0100

    Prepare for PyPI upload

--- setup.py
+++ setup.py
@@ -2,10 +2,27 @@ from setuptools import setup, find_packages
  
 setup(
     name="pyinstrument",
-    version="0.7",
-    description="A call stack profiler for Python. Inspired by Apple's Instruments.app",
-    long_description=open('README.md').read(),
     packages=['pyinstrument'],
+    version="0.10",
+    description="A call stack profiler for Python. Inspired by Apple's Instruments.app",
+    author='Joe Rickerby',
+    author_email='joerick@mac.com',
+    url='https://github.com/joerick/pyinstrument',
+    keywords=['profiling', 'profile', 'profiler', 'cpu', 'time'],
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Testing',
+    ]
 )
