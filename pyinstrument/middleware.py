bc0db3560b7b35915886ae748c59a8026585e1f9
Joe Rickerby <joerick@mac.com>
2014-07-09 22:37:25 +0100

    Add PYINSTRUMENT_USE_SIGNAL option to Django middleware
    For cases when signals interfere with IO. For me the problem was with
    boto, which uses outgoing network requests.

--- pyinstrument/middleware.py
+++ pyinstrument/middleware.py
@@ -4,34 +4,35 @@ from pyinstrument import Profiler
 from pyinstrument.profiler import NotMainThreadError
 
 not_main_thread_message = (
-    'pyinstrument can only be used on the main thread. Run your server process in single-threaded '
-    'mode. \n\nWith the built-in server, you can do this with '
-    '\n./manage.py runserver --nothreading --noreload')
+    'pyinstrument can only be used on the main thread in signal mode. Run your server process in '
+    'single-threaded mode. \n\n'
+    'With the built-in server, you can do this with\n'
+    './manage.py runserver --nothreading --noreload\n\n'
+    "Alternatively, you can set 'PYINSTRUMENT_USE_SIGNAL = False' in your settings.py to run in"
+    "'setprofile' mode. For more information, see\n"
+    'https://github.com/joerick/pyinstrument#signal-or-setprofile-mode')
 
 
 class ProfilerMiddleware(object):
-    def __init__(self):
-        self.profiler = None
-
     def process_request(self, request):
+        use_signal = getattr(settings, 'PYINSTRUMENT_USE_SIGNAL', True)
+
         if getattr(settings, 'PYINSTRUMENT_URL_ARGUMENT', 'profile') in request.GET:
-            self.profiler = Profiler()
+            profiler = Profiler(use_signal=use_signal)
             try:
-                self.profiler.start()
+                profiler.start()
+                request.profiler = profiler
             except NotMainThreadError:
                 raise NotMainThreadError(not_main_thread_message)
-                self.profiler = None
 
 
     def process_response(self, request, response):
-        if self.profiler:
+        if hasattr(request, 'profiler'):
             try:
-                self.profiler.stop()
+                request.profiler.stop()
 
-                return HttpResponse(self.profiler.output_html())
+                return HttpResponse(request.profiler.output_html())
             except NotMainThreadError:
                 raise NotMainThreadError(not_main_thread_message)
-            finally:
-                self.profiler = None
         else:
             return response
