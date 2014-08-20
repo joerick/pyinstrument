94b89f9a13a44a5a4b244c9aac9a7b81a4359ce5
Joe Rickerby <joerick@mac.com>
2014-08-20 21:40:50 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pyinstrument",
     packages=['pyinstrument'],
-    version="0.13",
+    version="0.13.1",
     description="A call stack profiler for Python. Inspired by Apple's Instruments.app",
     author='Joe Rickerby',
     author_email='joerick@mac.com',
