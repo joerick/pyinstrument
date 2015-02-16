a0b04791385989a2b3dc92892a3b78b4102b31f8
Joe Rickerby <joerick@mac.com>
2015-02-16 18:13:15 +0000

    Add a couple of test cases

---
+++ tests/overflow.py
@@ -0,0 +1,23 @@
+from pyinstrument import Profiler
+
+p = Profiler(use_signal=False)
+
+p.start()
+
+def func(num):
+    if num == 0:
+        return
+    b = 0
+    for x in range(1,100000):
+        b += x
+
+    return func(num - 1)
+
+func(900)
+
+p.stop()
+
+print(p.output_text())
+
+with open('overflow_out.html', 'w') as f:
+    f.write(p.output_html())
