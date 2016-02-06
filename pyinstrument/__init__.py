529cb6e456a593895ca27d1b572c18828f8cb302
Maximilian Roos <m@maxroos.com>
2016-02-06 02:15:14 -0500

    context manager & ipython magic

--- pyinstrument/__init__.py
+++ pyinstrument/__init__.py
@@ -1,1 +1,7 @@
-from pyinstrument.profiler import Profiler
+from pyinstrument.profiler import Profiler, instrument_profile, PyInstrumentMagic
+
+
+# TODO: ensure this doesn't break if ipython isn't available
+from IPython import get_ipython
+ip = get_ipython()
+ip.register_magics(PyInstrumentMagic)
