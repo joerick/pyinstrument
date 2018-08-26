4c5b2c01c73ca6f19544837628463c8771a382ff
Joe Rickerby <joerick@mac.com>
2018-08-26 18:15:54 +0100

    comment

--- test/test_overflow.py
+++ test/test_overflow.py
@@ -18,7 +18,7 @@ def test_profiler_doesnt_overflow_on_large_call_stacks():
     profiler = Profiler()
     profiler.start()
 
-    # give ourselves 170 frames of leeway to do our work.
+    # give 170 frames of leeway for the test runner and for pyinstrument to do its work.
     recursion_depth = sys.getrecursionlimit() - 170
     recurse(recursion_depth)
 
