b0a20bc03ad2ca3d50326e9af46f5e7636bcacda
Joe Rickerby <joerick@mac.com>
2018-11-18 20:18:05 +0000

    Add tests for all the processors

--- pyinstrument/processors.py
+++ pyinstrument/processors.py
@@ -140,6 +140,7 @@ def remove_unnecessary_self_time_nodes(frame, options):
     
     return frame
 
+
 def remove_irrelevant_nodes(frame, options, total_time=None):
     '''
     Remove nodes that represent less than e.g. 1% of the output
--- test/test_processors.py
+++ test/test_processors.py
@@ -1,3 +1,4 @@
+import sys
 from pyinstrument import processors
 from pyinstrument.frame import Frame, SelfTimeFrame
 from pytest import approx
@@ -142,3 +143,142 @@ def test_aggregate_repeated_calls():
     assert frame.children[0].children[0].function == 'scan_string'
     assert isinstance(frame.children[1], SelfTimeFrame)
     assert frame.children[1].time() == approx(0.35)
+
+
+def test_remove_irrelevant_nodes():
+    frame = Frame(
+        identifier='<module>\x00cibuildwheel/__init__.py\x0012',
+        children=[
+            Frame(
+                identifier='strip_newlines\x00cibuildwheel/utils.py\x00997',
+                children=[
+                    Frame(
+                        identifier='scan_string\x00cibuildwheel/utils.py\x0054',
+                        self_time=10,
+                    ),
+                ]
+            ),
+            SelfTimeFrame(
+                self_time=0.5,
+            ),
+            Frame(
+                identifier='strip_newlines\x00cibuildwheel/utils.py\x00997',
+                self_time=0.5,
+            ),
+            Frame(
+                identifier='calculate_metrics\x00cibuildwheel/utils.py\x007',
+                self_time=0.01,
+            ),
+        ]
+    )
+
+    assert frame.time() == approx(11.01)
+
+    frame = processors.remove_irrelevant_nodes(frame, options={})
+
+    assert frame.time() == approx(11.01)
+    # check the calculate metrics function was deleted
+    assert len(frame.children) == 3
+    assert 'calculate_metrics' not in [f.function for f in frame.children]
+
+
+def test_remove_unnecessary_self_time_nodes():
+    frame = Frame(
+        identifier='<module>\x00cibuildwheel/__init__.py\x0012',
+        children=[
+            Frame(
+                identifier='strip_newlines\x00cibuildwheel/utils.py\x00997',
+                children=[
+                    SelfTimeFrame(
+                        self_time=0.2,
+                    ),
+                ]
+            ),
+            SelfTimeFrame(
+                self_time=0.5,
+            ),
+            Frame(
+                identifier='strip_newlines\x00cibuildwheel/utils.py\x00997',
+                self_time=0.5,
+            ),
+            Frame(
+                identifier='calculate_metrics\x00cibuildwheel/utils.py\x007',
+                self_time=0.1,
+            ),
+        ]
+    )
+
+    assert frame.time() == approx(1.3)
+
+    frame = processors.remove_unnecessary_self_time_nodes(frame, options={})
+
+    assert frame.time() == approx(1.3)
+    assert len(frame.children) == 4
+    # check the self time node was deleted
+    strip_newlines_frame = frame.children[0]
+    assert strip_newlines_frame.function == 'strip_newlines'
+    assert len(strip_newlines_frame.children) == 0
+    assert strip_newlines_frame.self_time == 0.2
+
+
+def test_group_library_frames_processor():
+    frame = Frame(
+        identifier='<module>\x00cibuildwheel/__init__.py\x0012',
+        children=[
+            Frame(
+                identifier='library_function\x00env/lib/python3.6/django/__init__.py\x00997',
+                children=[
+                    Frame(
+                        identifier='library_inner\x00env/lib/python3.6/django/http.py\x0054',
+                        children=[
+                            Frame(
+                                identifier='library_callback\x00env/lib/python3.6/django/views.py\x0054',
+                                children=[
+                                    Frame(
+                                        identifier='<module>\x00cibuildwheel/views.py\x0012',
+                                        self_time=0.3,
+                                    ),
+                                ]
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+            SelfTimeFrame(
+                self_time=0.5,
+            ),
+            Frame(
+                identifier='strip_newlines\x00cibuildwheel/utils.py\x00997',
+                self_time=0.5,
+            ),
+            Frame(
+                identifier='calculate_metrics\x00cibuildwheel/utils.py\x007',
+                self_time=0.1,
+            ),
+        ]
+    )
+
+    assert frame.time() == approx(1.4)
+
+    frame = processors.group_library_frames_processor(frame, options={})
+
+    assert frame.time() == approx(1.4)
+    group_root = frame.children[0]
+
+    group = group_root.group
+    assert group.root == group_root
+
+    for frame in group.frames:
+        assert frame.group == group
+
+    assert group_root in group.frames
+    assert group_root.children[0] in group.frames
+    assert group_root.children[0].children[0] in group.frames
+    assert group_root.children[0].children[0] in group.exit_frames
+    assert group_root.children[0].children[0].children[0] not in group.frames
+    
+    old_sys_path = sys.path[:]
+    sys.path.append('env/lib/python3.6')
+    assert group.libraries == ['django']
+    sys.path[:] = old_sys_path
+
