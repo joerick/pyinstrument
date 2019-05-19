11edde7a51939a586f33e64de03530092d224906
Michael OBrien <obrienmd@gmail.com>
2019-05-19 06:55:53 -0700

    Make output file path swap ? for _qs_ Windows-only

--- pyinstrument/middleware.py
+++ pyinstrument/middleware.py
@@ -30,8 +30,12 @@ class ProfilerMiddleware(MiddlewareMixin):
 
             # Limit the length of the file name (255 characters is the max limit on major current OS, but it is rather
             # high and the other parts (see line 36) are to be taken into account; so a hundred will be fine here).
-            path = request.get_full_path().replace('/', '_').replace('?', '_qs_')[:100]
-
+            path = request.get_full_path().replace('/', '_')[:100]
+            
+            # Swap out ? for _qs_ on Windows, where ? cannot be used in file names.
+            if platform.platform.startswith('Windows'):
+                path = path.replace('?', '_qs_')
+            
             if profile_dir:
                 filename = '{total_time:.3f}s {path} {timestamp:.0f}.html'.format(
                     total_time=request.profiler.root_frame().time(),
