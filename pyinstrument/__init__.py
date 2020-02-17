757dd11c3d6f8aaa3f775fe831fc665577038f19
Joe Rickerby <joerick@mac.com>
2020-02-17 21:44:30 +0000

    Bump version

--- pyinstrument/__init__.py
+++ pyinstrument/__init__.py
@@ -1,7 +1,7 @@
 from pyinstrument.profiler import Profiler
 import warnings
 
-__version__ = '3.1.1'
+__version__ = '3.1.2'
 
 # enable deprecation warnings
 warnings.filterwarnings("once", ".*", DeprecationWarning, r"pyinstrument\..*")
