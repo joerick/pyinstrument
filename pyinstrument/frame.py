f057281e18c03da665fde4b0695dc0592961393d
Joe Rickerby <joerick@mac.com>
2020-02-17 18:56:19 +0000

    functions that live in <__array_function__ internals> are not app code

--- pyinstrument/frame.py
+++ pyinstrument/frame.py
@@ -175,8 +175,15 @@ class Frame(BaseFrame):
     @property
     def is_application_code(self):
         if self.identifier:
-            return (('%slib%s' % (os.sep, os.sep)) not in self.file_path
-                    and '<frozen importlib._bootstrap' not in self.file_path)
+            file_path = self.file_path
+
+            if ('%slib%s' % (os.sep, os.sep)) in file_path:
+                return False
+
+            if file_path.startswith('<') and file_path.endswith('>'):
+                return False
+
+            return True
 
     @property
     def code_position_short(self):
