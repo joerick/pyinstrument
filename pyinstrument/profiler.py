56f8da9a923205d8a4306a5ab5df182d2d97da13
Joe Rickerby <joerick@mac.com>
2021-04-12 09:49:01 +0100

    Add the stack sampler object, which handles fan-out for multiple Profilers

--- pyinstrument/profiler.py
+++ pyinstrument/profiler.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+from typing import Union
+from pyinstrument.stack_sampler import build_call_stack, get_stack_sampler
 import timeit, time, sys, inspect
 from contextvars import ContextVar
 from pyinstrument import renderers
 from pyinstrument.session import ProfilerSession
-from pyinstrument.util import deprecated, deprecated_option, file_supports_color, file_supports_unicode
-from pyinstrument_cext import setstatprofile
+from pyinstrument.util import file_supports_color, file_supports_unicode
 
 try:
     from time import process_time
@@ -14,14 +15,11 @@ except ImportError:
 timer = timeit.default_timer
 
 
-active_profiler_context_var = ContextVar('active_profiler_context_var', default=None)
+active_profiler_context_var: ContextVar[Union['Profiler', None]] = ContextVar('active_profiler_context_var', default=None)
 
 
 class Profiler(object):
-    # pylint: disable=W0613
-    @deprecated_option('use_signal')
-    @deprecated_option('recorder')
-    def __init__(self, interval=0.001, use_signal=None, recorder=None):
+    def __init__(self, interval=0.001):
         self.interval = interval
         self.last_profile_time = 0.0
         self.frame_records = []
@@ -31,22 +29,20 @@ class Profiler(object):
         self.context_var_token = None
 
     def start(self, caller_frame=None):
-        self.last_profile_time = timer()
         self._start_time = time.time()
+
         if process_time:
             self._start_process_time = process_time()
         if caller_frame is None:
             caller_frame = inspect.currentframe().f_back
-        self._start_call_stack = self._call_stack_for_frame(caller_frame)
 
-        if sys.getprofile() is not None:
-            raise RuntimeError('A profiler is already running. Running multiple profilers on the same thead is not supported.')
+        self._start_call_stack = build_call_stack(caller_frame, 'initial', None)
 
         self.context_var_token = active_profiler_context_var.set(self)
-        setstatprofile(self._profile, self.interval)
+        get_stack_sampler().subscribe(self, self.interval)
 
     def stop(self):
-        setstatprofile(None)
+        get_stack_sampler().unsubscribe(self)
         active_profiler_context_var.reset(self.context_var_token)
 
         if process_time:
@@ -75,43 +71,12 @@ class Profiler(object):
         self.stop()
 
     # pylint: disable=W0613
-    def _profile(self, frame, event, arg):
-        now = timer()
-        time_since_last_profile = now - self.last_profile_time
-
+    def sampler_saw_call_stack(self, call_stack, time_since_last_sample):
         if active_profiler_context_var.get() is self:
-            if event == 'call':
-                frame = frame.f_back
-
-            call_stack = self._call_stack_for_frame(frame)
-
-            if event == 'c_return' or event == 'c_exception':
-                c_frame_identifier = '%s\x00%s\x00%i' % (
-                    getattr(arg, '__qualname__', arg.__name__), '<built-in>', 0
-                )
-                call_stack.append(c_frame_identifier)
-
-            self.frame_records.append((call_stack, time_since_last_profile))
+            self.frame_records.append((call_stack, time_since_last_sample))
         else:
             # we have left the async context where this profiler was started.
-            self.frame_records.append(([Profiler.OUT_OF_CONTEXT_FRAME_IDENTIFIER], time_since_last_profile))
-
-        self.last_profile_time = now
-
-    def _call_stack_for_frame(self, frame):
-        call_stack = []
-
-        while frame is not None:
-            identifier = '%s\x00%s\x00%i' % (
-                frame.f_code.co_name, frame.f_code.co_filename, frame.f_code.co_firstlineno
-            )
-            call_stack.append(identifier)
-            frame = frame.f_back
-
-        # we iterated from the leaf to the root, we actually want the call stack
-        # starting at the root, so reverse this array
-        call_stack.reverse()
-        return call_stack
+            self.frame_records.append(([Profiler.OUT_OF_CONTEXT_FRAME_IDENTIFIER], time_since_last_sample))
 
     OUT_OF_CONTEXT_FRAME_IDENTIFIER = '<out-of-context>\x00<out-of-context>\x000'
 
@@ -131,46 +96,14 @@ class Profiler(object):
             file=file,
         )
 
-    @deprecated_option('root')
-    def output_text(self, root=None, unicode=False, color=False, show_all=False, timeline=False):
+    def output_text(self, unicode=False, color=False, show_all=False, timeline=False):
         return renderers.ConsoleRenderer(unicode=unicode, color=color, show_all=show_all, timeline=timeline).render(self.last_session)
 
-    @deprecated_option('root')
-    def output_html(self, root=None, timeline=False):
+    def output_html(self, timeline=False):
         return renderers.HTMLRenderer(timeline=timeline).render(self.last_session)
 
     def open_in_browser(self, timeline=False):
         return renderers.HTMLRenderer(timeline=timeline).open_in_browser(self.last_session)
 
-    @deprecated_option('root')
-    def output(self, renderer, root=None):
+    def output(self, renderer):
         return renderer.render(self.last_session)
-
-    @deprecated
-    def root_frame(self):
-        if self.last_session:
-            return self.last_session.root_frame()
-
-    @deprecated
-    def first_interesting_frame(self):
-        """
-        Traverse down the frame hierarchy until a frame is found with more than one child
-        """
-        root_frame = self.root_frame()
-        frame = root_frame
-
-        while len(frame.children) <= 1:
-            if frame.children:
-                frame = frame.children[0]
-            else:
-                # there are no branches
-                return root_frame
-
-        return frame
-
-    @deprecated
-    def starting_frame(self, root=False):
-        if root:
-            return self.root_frame()
-        else:
-            return self.first_interesting_frame()
---
+++ pyinstrument/stack_sampler.py
@@ -0,0 +1,112 @@
+import threading
+import timeit
+import types
+from typing import Any, List, NamedTuple, Optional, Union
+
+from pyinstrument_cext import setstatprofile
+
+thread_locals = threading.local()
+timer = timeit.default_timer
+
+
+class StackSamplerSubscriber(NamedTuple):
+    target: Any
+    desired_interval: float
+
+
+class StackSampler:
+    """ Manages setstatprofile for Profilers on a single thread """
+
+    subscribers: List[StackSamplerSubscriber] = []
+    current_sampling_interval: Optional[float] = None
+    last_profile_time: float = 0.0
+
+    def subscribe(self, subscriber, desired_interval):
+        self.subscribers.append(
+            StackSamplerSubscriber(target=subscriber, desired_interval=desired_interval)
+        )
+        self._update()
+
+    def unsubscribe(self, subscriber):
+        self.subscribers = [s for s in self.subscribers if s.target is not subscriber]
+        self._update()
+
+    def _update(self):
+        if len(self.subscribers) == 0:
+            self._stop_sampling()
+            return
+
+        min_subscribers_interval = min(s.desired_interval for s in self.subscribers)
+
+        if self.current_sampling_interval != min_subscribers_interval:
+            self._start_sampling(interval=min_subscribers_interval)
+
+    def _start_sampling(self, interval):
+        self.current_sampling_interval = interval
+        if self.last_profile_time == 0.0:
+            self.last_profile_time = timer()
+        setstatprofile(self._sample, interval)
+
+    def _stop_sampling(self):
+        setstatprofile(None)
+        self.current_sampling_interval = None
+        self.last_profile_time = 0.0
+
+    def _sample(self, frame, event, arg):
+        now = timer()
+        time_since_last_sample = now - self.last_profile_time
+
+        call_stack = build_call_stack(frame, event, arg)
+
+        for subscriber in self.subscribers:
+            subscriber.target.sampler_saw_call_stack(call_stack, time_since_last_sample)
+
+        self.last_profile_time = now
+
+
+def get_stack_sampler() -> StackSampler:
+    """
+    gets the stack sampler for the current thread, creating it if necessary
+    """
+    if not hasattr(thread_locals, "stack_sampler"):
+        thread_locals.stack_sampler = StackSampler()
+    return thread_locals.stack_sampler
+
+
+def build_call_stack(
+    frame: Union[types.FrameType, None], event: str, arg: Any
+) -> List[str]:
+    call_stack = []
+
+    if event == "call":
+        # if we're entering a function, the time should be attributed to
+        # the caller
+        frame = frame.f_back
+    elif event == "c_return" or event == "c_exception":
+        # if we're exiting a C function, we should add a frame before
+        # any Python frames that attributes the time to that C function
+        c_frame_identifier = "%s\x00%s\x00%i" % (
+            getattr(arg, "__qualname__", arg.__name__),
+            "<built-in>",
+            0,
+        )
+        call_stack.append(c_frame_identifier)
+
+    while frame is not None:
+        identifier = "%s\x00%s\x00%i" % (
+            frame.f_code.co_name,
+            frame.f_code.co_filename,
+            frame.f_code.co_firstlineno,
+        )
+        call_stack.append(identifier)
+        frame = frame.f_back
+
+    thread = threading.current_thread()
+    thread_identifier = "%s\x00%s\x00%i" % (thread.name, "<thread>", thread.ident)
+    call_stack.append(thread_identifier)
+
+    # we iterated from the leaf to the root, we actually want the call stack
+    # starting at the root, so reverse this array
+    call_stack.reverse()
+
+    return call_stack
