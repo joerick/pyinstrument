da9cad47d6925abb6678dabbe2cd8e2e0188147b
Grisha K <naspeh@gmail.com>
2021-10-18 15:50:53 +0300

    Fix failed test (test_profiler_task_isolation)

--- test/test_profiler_async.py
+++ test/test_profiler_async.py
@@ -156,8 +156,7 @@ def test_profiler_task_isolation(engine):
 
     root_frame = profiler_session.root_frame()
     assert root_frame is not None
-    fake_work_frame = next(f for f in walk_frames(root_frame) if f.function == "async_wait")
-    assert fake_work_frame.time() == pytest.approx(0.1 + 0.5, rel=0.1)
+    assert root_frame.time() == pytest.approx(0.1 + 0.5, rel=0.1)
 
     root_frame = processors.aggregate_repeated_calls(root_frame, {})
     assert root_frame
