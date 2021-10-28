826f310f91936933dfd4f17f5396cfa03852aad5
Geoffrey M Oxberry <goxberry@gmail.com>
2021-10-27 23:15:58 -0700

    SpeedscopeRenderer: omit repeated call aggregator
    This commit deletes the processor.aggregate_repeated_calls method from
    the list of default processors returned by SpeedscopeRenderer because
    speedscope is a timeline-based format, and aggregating repeated calls
    fouls up a timeline view.

--- pyinstrument/renderers/speedscope.py
+++ pyinstrument/renderers/speedscope.py
@@ -210,10 +210,14 @@ class SpeedscopeRenderer(Renderer):
         return "%s\n" % json.dumps(speedscope_file, cls=SpeedscopeEncoder)
 
     def default_processors(self) -> ProcessorList:
+        """
+        Default Processors for speedscope renderer; note that
+        processors.aggregate_repeated_calls is removed because
+        speedscope is a timeline-based format.
+        """
         return [
             processors.remove_importlib,
             processors.merge_consecutive_self_time,
-            processors.aggregate_repeated_calls,
             processors.group_library_frames_processor,
             processors.remove_unnecessary_self_time_nodes,
             processors.remove_irrelevant_nodes,
