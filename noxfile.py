39d67791d4e22cd0bbc55acea33b1af6bdef9a11
Joe Rickerby <joerick@mac.com>
2024-07-29 17:05:39 +0100

    Drop support for 3.7

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.7", "3.8", "3.9"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def test(session):
     session.install("-r", "requirements-dev.txt")
     session.run("pytest")
--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         ],
     },
     include_package_data=True,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     entry_points={"console_scripts": ["pyinstrument = pyinstrument.__main__:main"]},
     zip_safe=False,
     classifiers=[
