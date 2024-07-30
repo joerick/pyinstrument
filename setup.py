2e02d6baa835e72e8b405e8fa010a5ef2064df54
Joe Rickerby <joerick@mac.com>
2024-07-30 09:40:15 +0100

    Version pin for py3.13

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
             "pytest",
             "flaky",
             "trio",
+            "cffi >= v1.17.0rc1 ; python_version >= '3.13'",  # trio dep, pinned to a version that works with py3.13
             "greenlet>=3.0.0a1 ; python_version < '3.13'",
             "greenlet @ https://github.com/vstinner/greenlet/archive/refs/heads/py313.zip ; python_version >= '3.13'",
             "pytest-asyncio==0.12.0",  # pinned to an older version due to an incompatibility with flaky
