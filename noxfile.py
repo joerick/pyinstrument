14fc4294975506551f39008e6bbe636380935ce5
Joe Rickerby <joerick@mac.com>
2024-07-30 16:52:24 +0100

    nox fixes for 3.13

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,13 @@
+import os
+
 import nox
+
+nox.needs_version = ">=2024.4.15"
+nox.options.default_venv_backend = "uv|virtualenv"
 
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
 def test(session):
+    session.env["UV_PRERELEASE"] = "allow"
     session.install("-e", ".[test]")
     session.run("pytest")
