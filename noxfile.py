ad234a0f2fb19fd12efd6ab4e5d6acd4a27d04ec
Joe Rickerby <joerick@mac.com>
2024-07-31 16:21:58 +0100

    uv-friendly nox setup

--- noxfile.py
+++ noxfile.py
@@ -9,5 +9,6 @@ nox.options.default_venv_backend = "uv|virtualenv"
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"])
 def test(session):
     session.env["UV_PRERELEASE"] = "allow"
-    session.install("-e", ".[test]")
+    session.install("-e", ".[test]", "setuptools")
+    session.run("python", "setup.py", "build_ext", "--inplace")
     session.run("pytest")
